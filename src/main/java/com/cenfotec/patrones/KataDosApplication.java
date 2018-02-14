f6db0d5c4679b76c55b88812e7c4c2199cfac271
D-Rod <danrodret@outlook.com>
2018-02-14 12:18:34 -0600

    Prueba Error

---
+++ src/main/java/com/cenfotec/patrones/KataDosApplication.java
@@ -0,0 +1,14 @@
+package com.cenfotec.patrones;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class KataDosApplication {
+	
+	public static void main(String[] args){
+		
+	
+	}
+}
---
+++ src/test/java/com/cenfotec/patrones/logica/LogicaFactorTest.java
@@ -0,0 +1,14 @@
+package com.cenfotec.patrones.logica;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LogicaFactorTest {
+
+	@Test
+	public void pruebaError() {
+		return 0;
+	}
+
+}
